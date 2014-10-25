aaa7224bcedf99f9619abe1d8ba235d62753edb2
Jimmy Yang <gankkank@gmail.com>
2014-10-25 22:20:26 +0800

    add css file to prevent https warning

--- lib/redmine_pretty_print/hooks/add_pretty.rb
+++ lib/redmine_pretty_print/hooks/add_pretty.rb
@@ -5,7 +5,8 @@ module RedminePrettyPrint
       def view_layouts_base_html_head(context = {})
         header = []
         header << javascript_include_tag("https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js")
-        header << stylesheet_link_tag("http://jmblog.github.com/color-themes-for-google-code-prettify/css/themes/github.css")
+        #header << stylesheet_link_tag("http://jmblog.github.com/color-themes-for-google-code-prettify/css/themes/github.css")
+        header << stylesheet_link_tag(:github, :plugin => "redmine_pretty_print")
         header.join(" ")
       end
 
