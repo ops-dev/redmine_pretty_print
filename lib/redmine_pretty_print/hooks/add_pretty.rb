20d4d3a62a93333cc4369ebc9a685568cd8466f3
Jimmy Yang <gankkank@gmail.com>
2014-11-04 02:09:59 +0800

    css before js

--- lib/redmine_pretty_print/hooks/add_pretty.rb
+++ lib/redmine_pretty_print/hooks/add_pretty.rb
@@ -4,10 +4,10 @@ module RedminePrettyPrint
 
       def view_layouts_base_html_head(context = {})
         header = []
+        header << stylesheet_link_tag(:hemisu_light, :plugin => "redmine_pretty_print")
         #header << javascript_include_tag("https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js")
         header << javascript_include_tag(:run_prettify, :plugin => "redmine_pretty_print")
         #header << stylesheet_link_tag("http://jmblog.github.com/color-themes-for-google-code-prettify/css/themes/github.css")
-        header << stylesheet_link_tag(:hemisu_light, :plugin => "redmine_pretty_print")
         header.join(" ")
       end
 
